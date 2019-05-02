1d00802e29279f2338144141e5e3d4e20d8be8aa
fenouil <fenouil@RFDELL>
2019-05-02 14:27:31 +0200

    Fixed workload quotes and defulat value for arg1

--- naiveH.sh
+++ naiveH.sh
@@ -9,14 +9,14 @@
 #### Parameters ####
 
 # Command line values
-HASH_FILE=${1:"example0.hash"}
-DICTFILE=${2:-"~/myDic.txt"}
+HASH_FILE=${1:-"example0.hash"}
+DICT_FILE=${2:-"~/myDic.txt"}
 HASH_TYPE=${3:-"0"}
 
 # Paths to hashcat files and dictionnary to be used
-HASHCAT_BIN=${HASHCAT_BIN:-"/usr/local/bin/hashcat"}
-HASHCAT_RULEFILES=( "d3ad0ne.rule" "rockyou-30000.rule" "dive.rule" )
-HASHCAT_MASKFILES=( "rockyou-1-60.hcmask" )
+HASHCAT_BIN=${HASHCAT_BIN:-"/$HOME/hashcat/hashcat"}
+HASHCAT_RULEFILES=( "$HOME/hashcat/rules/d3ad0ne.rule" "$HOME/hashcat/rules/rockyou-30000.rule" "$HOME/hashcat/rules/dive.rule" )
+HASHCAT_MASKFILES=( "$HOME/hashcat/masks/rockyou-1-60.hcmask" )
 
 # How hard is the GPU going to be hit
 WORKLOAD="-w 4"
@@ -38,7 +38,7 @@ echo -e "-----------------------------------------"
 
 # LIGHT
 echo -e "\n$(date $dateFormat) -- DICTIONARY ATTACK -------------------------------------------"
-"$HASHCAT_BIN" "$WORKLOAD" --hash-type "$HASH_TYPE" --attack-mode 0 "$HASH_FILE" "$DICTFILE" --potfile-path "${OUTPUT_PREFIX}.pot" --outfile "${OUTPUT_PREFIX}.out"
+"$HASHCAT_BIN" $WORKLOAD --hash-type "$HASH_TYPE" --attack-mode 0 "$HASH_FILE" "$DICT_FILE" --potfile-path "${OUTPUT_PREFIX}.pot" --outfile "${OUTPUT_PREFIX}.out"
 
 
 # MEDIUM
@@ -46,7 +46,7 @@ echo -e "\n$(date $dateFormat) -- DICTIONARY ATTACK WITH RULES -----------------
 for currentRuleFile in "${HASHCAT_RULEFILES[$@]}"
 do
 	echo -e "$(date $dateFormat) --> Using rule file: ${currentRuleFile}"
-	"$HASHCAT_BIN" "$WORKLOAD" --hash-type "$HASH_TYPE" --attack-mode 0 "$HASH_FILE" "$DICTFILE" --rules-file "$currentRuleFile" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
+	"$HASHCAT_BIN" $WORKLOAD --hash-type "$HASH_TYPE" --attack-mode 0 "$HASH_FILE" "$DICT_FILE" --rules-file "$currentRuleFile" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
 done
 
 
@@ -55,13 +55,13 @@ echo -e "\n$(date $dateFormat) -- MASK ATTACK (BRUTE-FORCE) --------------------
 for currentMaskFile in "${HASHCAT_MASKFILES[$@]}"
 do
 	echo -e "$(date $dateFormat) --> Using mask file: ${currentMaskFile}"
-	"$HASHCAT_BIN" "$WORKLOAD" --hash-type "$HASH_TYPE" --attack-mode 3 "$HASH_FILE" "$currentMaskFile" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
+	"$HASHCAT_BIN" $WORKLOAD --hash-type "$HASH_TYPE" --attack-mode 3 "$HASH_FILE" "$currentMaskFile" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
 done
 
 
 # This one can take 12+ hours...
 echo -e "\n$(date $dateFormat) -- COMBINATION ATTACK ------------------------------------------"
-"$HASHCAT_BIN" "$WORKLOAD" --hash-type "$HASH_TYPE" --attack-mode 1 "$HASH_FILE" "$DICTFILE" "$DICTFILE" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
+"$HASHCAT_BIN" $WORKLOAD --hash-type "$HASH_TYPE" --attack-mode 1 "$HASH_FILE" "$DICT_FILE" "$DICT_FILE" --potfile-path "$POT_FILENAME" --outfile "${OUTPUT_PREFIX}.out"
 
 
 echo -e "\n-----------------------------------------"
